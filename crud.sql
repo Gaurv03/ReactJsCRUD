99709da703a19f305c988eace3885072dc98d283
Gaurav Upadhyay <74143382+Gaurv03@users.noreply.github.com>
2023-02-12 22:09:52 +0530

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2023 at 05:19 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flags`
+--
+
+CREATE TABLE `flags` (
+  `id` int(11) NOT NULL,
+  `country` text NOT NULL,
+  `capital` text NOT NULL,
+  `flag` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `flags`
+--
+
+INSERT INTO `flags` (`id`, `country`, `capital`, `flag`) VALUES
+(1, 'India', 'New Delhi', ''),
+(3, 'Australia', 'Canberra', ''),
+(6, 'Morocco', 'Rabat', ''),
+(7, 'Brazil', 'Brasilia', ''),
+(8, 'Belarus', 'Minsk', ''),
+(9, 'Germany', 'Berlin', ''),
+(22, 'Albania', 'Canberra', '1676209628075_Belarus.avif');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `flags`
+--
+ALTER TABLE `flags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `flags`
+--
+ALTER TABLE `flags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
